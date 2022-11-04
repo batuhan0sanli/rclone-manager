1d39a37a7929c3bb44bcec4cb82ca196aa68f705
Batuhan Şanlı <batuhansanli@gmail.com>
2022-11-04 23:08:03 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from os import path
+
+# The directory containing this file
+PATH = path.abspath(path.dirname(__file__))
+
+setup(
+    name='rclone-manager',
+    version='0.0.1',
+    description='Define multiple tasks using rclone',
+    long_description='Define multiple tasks using rclone',
+    long_description_content_type='text/markdown',
+    url='https://github.com/batuhan0sanli/rclone-manager',
+    author='batuhan0sanli',
+    author_email='batuhansanli@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='rclone manager sync copy move file',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires='>=3.8',
+)
